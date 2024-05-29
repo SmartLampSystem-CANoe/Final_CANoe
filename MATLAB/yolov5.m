633a05189a39300dc9d323f9fdf27b59b222513f
kmugen <khj199859@gmail.com>
2024-05-30 00:30:14 +0900

    df

---
+++ MATLAB/yolov5.m
@@ -0,0 +1,31 @@
+
+name = 'tiny-yolov3-coco';
+detector = yolov3ObjectDetector(name);
+img = imread('left-night-HB.png');
+img = preprocess(detector,img);
+img = im2single(img);
+[bboxes,scores,labels] = detect(detector,img,'DetectionPreprocessing','none');
+
+sz = size(bboxes);
+sz = sz(1);
+
+if sz > 0
+    detectedImg = insertObjectAnnotation(img,'Rectangle',bboxes(1, :),scores(1, 1));
+
+    a = 1;
+    for i = 2:sz
+        if bboxes(i, 3) < 40
+            continue;
+        end
+    
+        if a == 1
+            a = 0;
+        else
+            detectedImg = insertObjectAnnotation(detectedImg,'Rectangle',bboxes(i, :),scores(i, 1));
+        end
+    
+    end
+
+    figure
+    imshow(detectedImg)
+end
